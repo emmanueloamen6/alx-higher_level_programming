fd123647ff38b188d901924b0badeb4b00296758
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-18 03:56:11 -0700

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1 and len(matrix) == 0:
         print("")
+    else:
     for arr in matrix:
         for n in arr:
             if n != arr[-1]:
