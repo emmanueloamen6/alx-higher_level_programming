8c6541ffa4ad828ee3e02a9fbf09a7075c826d1b
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-18 20:10:22 -0700

    Data structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     big = my_list[0]
     for n in range(len(my_list)):
-        if len(my_list) > big:
+        if my_list[n] > big:
             big = my_list[n]
     return (big)
