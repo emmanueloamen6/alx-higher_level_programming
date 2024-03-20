340b6bac1f3506e3d762b40033ded759d7836db7
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-20 13:06:29 -0700

    Data and structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[i ** 2 for i in row] for row in matrix]
-    return new_matrix
+    return [[y ** 2 for y in x] for x in matrix]
