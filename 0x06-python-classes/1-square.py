e6e0298d0e9ee85d06e2256d10fc005b8fa5e1a1
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-02 13:53:59 -0700

    Initialization

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""defination of class."""
+class Square:
+    """Represent class."""
+    def __init__(self, size):
+        """initialization of square.
+
+        argv:
+            size (int): The size of the new square.
+            """
+        self.__size = size
