7f7fc9c182af0c03326dbfc8de9990b209a79b3b
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-03 10:07:56 -0700

    Division of two lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    """ function that divides element by element 2 lists.
+    """Divides two lists element by element.
 
-        Args:
-            my_list_1 (list): list of elements.
-            my_list_2 (list): list of elements.
-            list_length (int): length of elements.
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
 
-        Returns: newlist
+    Returns:
+        A new list of length list_length containing all the divisions.
     """
     new_list = []
     result = 0
