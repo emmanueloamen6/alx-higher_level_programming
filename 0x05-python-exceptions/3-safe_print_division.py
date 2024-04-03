16d8de9790c982f3b74287c0b80966ceb926fd4b
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-03 07:25:13 -0700

    Division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """Function that divides 2 integers and prints the result.
+    """
+    Function that divides 2 integers and prints the result.
 
     Args:
         a (int): first integer.
