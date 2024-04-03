d097b1fb87ab2b3059f9455afb63bccc1a80f44e
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-03 02:35:22 -0700

    Square os area

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-"""define a Square"""
+"""Define a class Square."""
+
+
 class Square:
-    """initialization of Square"""
+    """Represent a square."""
+
     def __init__(self, size):
         """Initialize a new square.
 
@@ -15,20 +18,21 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Return the current Square area."""
-        return (self.__size * self__size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
     def my_print(self):
-        """Prints in stdout the square with the # character."""
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
