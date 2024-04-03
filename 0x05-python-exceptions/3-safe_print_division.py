e2fd0316811e2dc74a565a578dd6e0432f5cae2f
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-03 07:20:59 -0700

    Division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """ function that divides 2 integers and prints the result.
+
+    Args:
+        a (int): first integer.
+        b (int): Second integer.
+
+    Return:
+        return the value.
+    """
+    try:
+        value = a / b
+    except:
+        value = None
+    finally:
+        print("Inside result: {}".format(value))
+    return (value)
