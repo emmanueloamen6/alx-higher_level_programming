d1d550e6db8f79d1ea9642bf131e63f1a6d578fa
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-15 13:13:38 -0700

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    '''Check if  the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class.
-    '''
+    '''Checks if an object is a true subclass of a class.'''
     return isinstance(obj, a_class) and type(obj) != a_class
