79ff35ee54d65a7c3ced094208a74337711ad5f0
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-16 04:58:31 -0700

    Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,13 @@
 """Define the function that writes a string to a text file."""
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8)
-    and returns the number of characters written.
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
+
     with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
