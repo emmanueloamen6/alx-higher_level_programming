010794f944455c05a2c73bad910552f869884603
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-16 14:47:26 -0700

    Python input/output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a Python class-to-JSON function."""
+import json
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
