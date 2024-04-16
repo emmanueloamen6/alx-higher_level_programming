185ff8b00fa196d56691ccaef24a658cf0d71f76
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-16 04:14:17 -0700

    Python output/input

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Define the tex reading function."""
 
 
 def read_file(filename=""):
+    """ Reads a text file (UTF8) and prints it to stdout.
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
