6f12d40773f87d18489df1d39b36b9e3474aca46
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-16 14:32:00 -0700

    Python input/output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Define JSON file reading function."""
-
 import json
+
+
 def load_from_json_file(filename):
     """Creates a python file from JSON file."""
     with open(filename) as f:
