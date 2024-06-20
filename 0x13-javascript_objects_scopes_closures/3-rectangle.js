3a973692db4d23a110e4b54e99fd7173fdc6b67d
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-06-20 16:33:02 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,10 +5,7 @@
  */
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.height = h;
-      this.width = w;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
