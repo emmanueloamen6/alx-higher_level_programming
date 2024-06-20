3297076a519da19d4b5c1f8cacf57edb56903ecd
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-06-20 16:20:02 -0700

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* a class Rectangle that defines a rectangle
+ * Create an instance method called print()
+ * that prints the rectangle using the character X
+ */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
