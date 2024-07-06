91edd72336e98859272aeff2d1199f1f93a8da48
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-07-06 14:05:08 -0700

    Web-server

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("ascii"))
+            print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code))
