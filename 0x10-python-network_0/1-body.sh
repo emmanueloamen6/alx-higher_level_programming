9fbfab543c3973e09c59d3d6202abebae675b0f0
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-07-09 08:13:50 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ $(curl -L -s -X HEAD -w "%{http_code)" "$1"} == '200' ]; then curl -Ls "$1"; fi
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
